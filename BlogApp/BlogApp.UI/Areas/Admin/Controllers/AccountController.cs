49d495d6b5be99399c2340ea1610a8cf76298c47
sefadogan <sefadogan7@gmail.com>
2019-03-17 17:25:42 +0300

    Created admin login page and RedirectToRequestUrl filter. And some edits.

---
+++ BlogApp/BlogApp.UI/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.BLL.UnitOfWork;
+using BlogApp.Model.DataModel;
+using BlogApp.Model.ViewModel;
+using BlogApp.UI.Extensions;
+using BlogApp.UI.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.UI.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        IUnitOfWork _uow;
+        IHttpContextAccessor _accessor;
+
+        public AccountController(IUnitOfWork uow, IHttpContextAccessor accessor)
+        {
+            _uow = uow;
+            _accessor = accessor;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [TypeFilter(typeof(RedirectToRequestUrl))]
+        public IActionResult Login(VMAccountLogin model)
+        {
+            User user = _uow.UserRepository.Get(x => x.Email == model.Email && x.Password == model.Password);
+            if (user == null)
+            {
+                TempData["ProcessResult"] = "Email address or password is incorrect!";
+                TempData["AlertType"] = "warning";
+                return View();
+            }
+
+            _accessor.HttpContext.Session.SetObject("loggedUser", user);
+            return RedirectToAction("List","User"); // Bu action çalıştıktan sonra zaten request url'e göre yönlendirme olacağı için buranın bir önemi yok.
+        }
+    }
+}
---
+++ BlogApp/BlogApp.UI/Filters/RedirectToRequestUrl.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.UI.Filters
+{
+    public class RedirectToRequestUrl
+    {
+        IHttpContextAccessor _accessor;
+
+        public RedirectToRequestUrl(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            string requestUrl = _accessor.HttpContext.Session.GetString("requestUrl");
+            if (requestUrl == null)
+                context.Result = new RedirectResult("/Admin/Account/Login");
+
+            context.Result = new RedirectResult(requestUrl);
+        }
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+    }
+}
