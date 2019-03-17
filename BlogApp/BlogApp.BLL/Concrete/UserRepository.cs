90f9586ac51ebc36ede5b10aa9e67a52fb32254b
sefadogan <sefadogan7@gmail.com>
2019-03-17 16:14:53 +0300

    Added user repository.

---
+++ BlogApp/BlogApp.BLL/Concrete/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.BLL.Abstract;
+using BlogApp.DAL;
+using BlogApp.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.BLL.Concrete
+{
+    public class UserRepository : BaseRepository<User, int, BlogAppContext>, IUserRepository
+    {
+        public UserRepository(BlogAppContext context) : base(context)
+        {
+        }
+    }
+}
