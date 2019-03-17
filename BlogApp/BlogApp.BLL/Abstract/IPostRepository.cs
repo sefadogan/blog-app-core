38d2ee85e73a3fa60ac3f6ff09a4cc449d529852
sefadogan <sefadogan7@gmail.com>
2019-03-17 16:17:22 +0300

    Added post repository.

---
+++ BlogApp/BlogApp.BLL/Abstract/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.DAL;
+using BlogApp.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.BLL.Abstract
+{
+    public interface IPostRepository: IBaseRepository<Post, int, BlogAppContext>
+    {
+    }
+}
---
+++ BlogApp/BlogApp.BLL/Concrete/PostRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.BLL.Abstract;
+using BlogApp.DAL;
+using BlogApp.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.BLL.Concrete
+{
+    public class PostRepository : BaseRepository<Post, int, BlogAppContext>, IPostRepository
+    {
+        public PostRepository(BlogAppContext context) : base(context)
+        {
+        }
+    }
+}
