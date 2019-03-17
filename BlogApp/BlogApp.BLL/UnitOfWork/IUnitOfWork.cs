8aca25e5444db3cbb5e409c841c092d8e19ce066
sefadogan <sefadogan7@gmail.com>
2019-03-17 16:32:26 +0300

    Implemented Unit of Work pattern for repositories.

---
+++ BlogApp/BlogApp.BLL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.BLL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.BLL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository UserRepository { get; }
+        IPostRepository PostRepository { get; }
+        IRoleRepository RoleRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+
+        bool Commit();
+    }
+}
---
+++ BlogApp/BlogApp.BLL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlogApp.BLL.Abstract;
+using BlogApp.BLL.Concrete;
+using BlogApp.DAL;
+
+namespace BlogApp.BLL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BlogAppContext _context;
+        private IUserRepository userRepository;
+        private IPostRepository postRepository;
+        private IRoleRepository roleRepository;
+        private ICategoryRepository categoryRepository;
+
+        public UnitOfWork(BlogAppContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository UserRepository => UserRepository ?? (userRepository = new UserRepository(_context));
+        public IPostRepository PostRepository => PostRepository ?? (postRepository = new PostRepository(_context));
+        public IRoleRepository RoleRepository => RoleRepository ?? (roleRepository = new RoleRepository(_context));
+        public ICategoryRepository CategoryRepository => CategoryRepository ?? (categoryRepository = new CategoryRepository(_context));
+
+        public bool Commit()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true; 
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+}
