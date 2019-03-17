b2177997e3ca9c7051231b37022dcd7d394413a6
sefadogan <sefadogan7@gmail.com>
2019-03-17 16:22:36 +0300

    Created category repository.

---
+++ BlogApp/BlogApp.BLL/Concrete/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.BLL.Abstract;
+using BlogApp.DAL;
+using BlogApp.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.BLL.Concrete
+{
+    public class CategoryRepository : BaseRepository<Category, int, BlogAppContext>, ICategoryRepository
+    {
+        public CategoryRepository(BlogAppContext context) : base(context)
+        {
+        }
+    }
+}
