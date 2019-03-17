328c2d648752434994c4a735a20533aa35be40f5
sefadogan <sefadogan7@gmail.com>
2019-03-17 16:53:54 +0300

    Created custom authorization and session extension method.

---
+++ BlogApp/BlogApp.UI/Extensions/SessionExtensionMethod.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.UI.Extensions
+{
+    public static class SessionExtensionMethod
+    {
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            string data = JsonConvert.SerializeObject(value);
+            session.SetString(key, data);
+        }
+
+        public static T GetObject<T>(this ISession session, string key) where T : class
+        {
+            string data = session.GetString(key);
+
+            return (String.IsNullOrEmpty(data)) ? null : (JsonConvert.DeserializeObject<T>(data));
+        }
+    }
+}
---
+++ BlogApp/BlogApp.UI/Filters/CustomAuthorization.cs
@@ -0,0 +1,37 @@
+﻿using BlogApp.Model.DataModel;
+using BlogApp.UI.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.UI.Filters
+{
+    public class CustomAuthorization : IActionFilter
+    {
+        IHttpContextAccessor _accessor;
+
+        public CustomAuthorization(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var loggedUser = _accessor.HttpContext.Session.GetObject<User>("loggedUser");
+            if (loggedUser == null)
+            {
+                string requestUrl = _accessor.HttpContext?.Request?.GetDisplayUrl();
+                _accessor.HttpContext.Session.SetString("requestUrl", requestUrl);
+                context.Result = new RedirectResult("/Admin/Account/Login");
+            }
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
