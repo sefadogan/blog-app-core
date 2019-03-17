b626d8546b36e349c6872148e555a583b0c53415
sefadogan <sefadogan7@gmail.com>
2019-03-17 16:19:47 +0300

    Added role repository.

---
+++ BlogApp/BlogApp.BLL/Concrete/RoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.BLL.Abstract;
+using BlogApp.DAL;
+using BlogApp.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.BLL.Concrete
+{
+    public class RoleRepository : BaseRepository<Role, int, BlogAppContext>, IRoleRepository
+    {
+        public RoleRepository(BlogAppContext context) : base(context)
+        {
+        }
+    }
+}
